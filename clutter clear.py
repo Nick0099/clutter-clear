b8e0c08820152a23a15659e2540c4654f0fd6824
Nick0099 <100011021+Nick0099@users.noreply.github.com>
2024-06-28 19:46:02 +0545

    Update clutter clear.py

--- clutter clear.py
+++ clutter clear.py
@@ -1,16 +1,10 @@
 import os
-
-#  # create file (remove comment to create 100 files )
-# for i in range(1,101):
-#   open(f"png/file{i}.txt","x")
-  
-
-  # rename file
-for i in range(0,100):
-   os.rename(f"png/file{i+1}.txt",f"png/{i+1}.png")
-  
-  
-#   # delete file (remove comment to delete all files)
-# for i in range(0,100):
-#    os.remove(f"png/{i+1}.png")
-  
+i=1
+for file in os.listdir("clutter"):
+  if file.endswith(".png"):  
+    print(f"{file}")
+    os.rename(f"clutter/{file}",f"clutter/{i}.png")
+    
+    i=i+1
+# for i in range(1,100):
+#    open (f"clutter/sfhjsakaefj{i}.png","x")
